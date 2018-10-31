6cce2e7417d2b783e5ea92b77fb97c8b39c568ec
yangjc <bupt_yjc@163.com>
2018-10-31 20:11:16 +0800

    add flat_read

--- util.py
+++ util.py
@@ -37,6 +37,14 @@ def load_poly(path):
     return vocab
 
 
+def flat_read(path, field):
+    nest_items = pd.read_csv(path, usecols=[field], keep_default_na=False).values
+    items = list()
+    for nest_item in nest_items:
+        items.append(nest_item[0])
+    return items
+
+
 def map_item(name, items):
     if name in items:
         return items[name]
