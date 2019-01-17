16667487909c9623bdb25d147b6018cb08bdc0be
yangjc <bupt_yjc@163.com>
2019-01-17 10:22:05 +0800

    change data

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,8 @@ import os
 
 import re
 
+from random import shuffle
+
 from util import load_word_re, load_type_re
 
 
@@ -19,29 +21,33 @@ def save(path, texts, labels):
             f.write(text + ',' + label + '\n')
 
 
-def prepare(path, path_dir):
+def prepare(path_univ_dir, path_train, path_test):
     text_set = set()
     texts, labels = list(), list()
-    files = os.listdir(path_dir)
+    files = os.listdir(path_univ_dir)
     for file in files:
         label = os.path.splitext(file)[0]
-        with open(os.path.join(path_dir, file), 'r') as f:
+        with open(os.path.join(path_univ_dir, file), 'r') as f:
             for line in f:
-                text = line.strip()
+                text = line.strip().lower()
                 text = re.sub(stop_word_re, '', text)
                 for word_type, word_re in word_type_re.items():
                     text = re.sub(word_re, word_type, text)
-                if text not in text_set:
+                if text and text not in text_set:
                     text_set.add(text)
                     texts.append(text)
                     labels.append(label)
-    save(path, texts, labels)
+    texts_labels = list(zip(texts, labels))
+    shuffle(texts_labels)
+    texts, labels = zip(*texts_labels)
+    bound = int(len(texts) * 0.9)
+    save(path_train, texts[:bound], labels[:bound])
+    save(path_test, texts[bound:], labels[bound:])
 
 
 if __name__ == '__main__':
-    path = 'data/train.csv'
-    path_dir = 'data/train'
-    prepare(path, path_dir)
-    path = 'data/test.csv'
-    path_dir = 'data/test'
-    prepare(path, path_dir)
+    path_univ_dir = 'data/univ'
+    path_train = 'data/train.csv'
+    path_test = 'data/test.csv'
+    prepare(path_univ_dir, path_train, path_test)
+
