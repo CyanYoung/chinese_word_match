a9230ec30a00586b7ed8e16cbf6cda89eabfc3d0
yangjc <bupt_yjc@163.com>
2019-03-23 16:26:06 +0800

    change format

--- eval.py
+++ eval.py
@@ -11,16 +11,15 @@ labels = flat_read(path_test, 'label')
 
 
 def test(name, texts, labels):
-    preds, errors = list(), list()
+    preds = list()
     for text, label in zip(texts, labels):
         pred = predict(text, name)
         preds.append(pred)
-        if pred != label:
-            errors.append((text, label, pred))
-    f1 = f1_score(labels, preds)
+    f1 = f1_score(labels, preds, average='weighted')
     print('\n%s f1: %.2f - acc: %.2f\n' % (name, f1, accuracy_score(labels, preds)))
-    for text, label, pred in errors:
-        print('{}: {} -> {}'.format(text, label, pred))
+    for text, label, pred in zip(texts, labels, preds):
+        if label != pred:
+            print('{}: {} -> {}'.format(text, label, pred))
 
 
 if __name__ == '__main__':
